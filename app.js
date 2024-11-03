d589fa579b26ff612610341927970d6de8ff998c
Wagner Ram <wagner19ramos@hotmail.com>
2024-11-03 16:41:02 -0300

    manipulando dados via rotas

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", "./views");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 const db = mysql.createConnection({
   host: "localhost",
   port: 3307,
@@ -27,8 +30,14 @@ db.connect((erro) => {
   console.log("Conectado ao banco de dados MySQL");
 });
 
-app.get("/", (req, res) => {
-  res.render("formulario");
+// renderizando pagina web
+app.get("/", (req, res) => res.render("formulario"));
+
+//rota de cadastro
+
+app.post("/cadastrar", (req, res) => {
+  console.log(req.body);
+  res.end();
 });
 
 app.listen(8080);
