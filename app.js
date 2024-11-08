eacd97927203269d158e3a5b95cab79971b05f36
Wagner Ram <wagner19ramos@hotmail.com>
2024-11-07 22:49:05 -0300

    correcao-adicionada

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// configuracao do banco
 const db = mysql.createConnection({
   host: "localhost",
   port: 3306,
